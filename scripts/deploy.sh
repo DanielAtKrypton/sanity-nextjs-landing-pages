4accf6eccd0ae36aee2b24689cefeddeecab6362
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-09-27 11:59:32 +0100

    :heavy_plus_sign: Add Travis CI.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git checkout master
+git merge -s ours $1 # Merge branches, but use our (=staging) branch head
 git checkout $1
-git reset --hard master
-git push -f origin $1
+git merge master
+git remote set-url origin $2
