c6fa203034c6239821e64c98dbbaf07a68724fdb
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-09-27 12:38:08 +0100

    :heavy_plus_sign: Add Travis CI.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,6 @@
 git merge -s ours origin $1 # Merge branches, but use our (=staging) branch head
 git checkout $1
 git merge master
+git commit -a -m "Machine deploy"
 git remote set-url origin "https://$2@github.com/krypton-unite/website.git"
+git push origin master
