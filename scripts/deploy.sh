0cdf3e3efc079312bbad55904862c163748f4a0c
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-09-27 10:01:42 +0100

    :heavy_plus_sign: Add Travis CI.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+
 if [ -n "$GITHUB_API_KEY" ]; then
     cd "$TRAVIS_BUILD_DIR"
-    git checkout production
+    git checkout $1
     git reset --hard master
     git -c user.name='travis' -c user.email='travis' commit -m init
     # Make sure to make the output quiet, or else the API token will leak!
     # This works because the API key can replace your password.
-    git push -f -q https://krypton-unite:$GITHUB_API_KEY@github.com/krypton-unite/website/production master &>/dev/null
+    git push -f -q https://krypton-unite:$GITHUB_API_KEY@github.com/krypton-unite/website/$1 master &>/dev/null
     cd "$TRAVIS_BUILD_DIR"
 fi
