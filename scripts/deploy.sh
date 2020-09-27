3983455445644d5d56edfb2d0e6bc2f66d746ecd
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-09-27 13:09:46 +0100

    :heavy_plus_sign: Add Travis CI.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,39 @@
 #!/bin/sh
+# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
-git merge -s ours $1 # Merge branches, but use our (=staging) branch head
-git checkout $1
-git merge master
-git commit -a -m "Machine deploy"
-git remote set-url origin "https://$2@github.com/krypton-unite/website.git"
-git push origin master
+setup_git() {
+  git config --global user.email "automatedtradesystem@kryptonunite.com"
+  git config --global user.name "MachineAtKrypton"
+}
+
+commit_country_json_files() {
+  git checkout master
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add -f dist/output/*.json
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://krypton-unite:$2@github.com/krypton-unite/website.git > /dev/null 2>&1
+  git push origin production --quiet
+}
+
+setup_git
+
+commit_country_json_files
+
+# Attempt to commit to git only if "git commit" succeeded
+if [ $? -eq 0 ]; then
+  echo "A new commit with changed country JSON files exists. Uploading to GitHub"
+  upload_files
+else
+  echo "No changes in country JSON files. Nothing to do"
+fi
