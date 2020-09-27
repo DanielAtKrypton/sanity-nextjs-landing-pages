16e7570f52534c54c4e024639cd5fea2227b6d4a
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-09-27 13:15:36 +0100

    :heavy_plus_sign: Add Travis CI.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,28 +2,28 @@
 # Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
 setup_git() {
-  git config --global user.email "automatedtradesystem@kryptonunite.com"
-  git config --global user.name "MachineAtKrypton"
+  git config --global user.email "automatedtradesystem@kryptonunite.com" > /dev/null 2>&1
+  git config --global user.name "MachineAtKrypton" > /dev/null 2>&1
 }
 
 commit_country_json_files() {
-  git checkout master
+  git checkout master > /dev/null 2>&1
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
   # Stage the modified files in dist/output
-  git add -f dist/output/*.json
+  git add -f dist/output/*.json > /dev/null 2>&1
   # Create a new commit with a custom build message
   # with "[skip ci]" to avoid a build loop
   # and Travis build number for reference
-  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]" > /dev/null 2>&1
 }
 
 upload_files() {
   # Remove existing "origin"
-  git remote rm origin
+  git remote rm origin > /dev/null 2>&1
   # Add new "origin" with access token in the git URL for authentication
   git remote add origin https://krypton-unite:$2@github.com/krypton-unite/website.git > /dev/null 2>&1
-  git push origin production --quiet
+  git push origin production --quiet > /dev/null 2>&1
 }
 
 setup_git
