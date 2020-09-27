de4dc295ccea05c3246f23e9952500f81daf2158
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-09-27 10:29:31 +0100

    :heavy_plus_sign: Add Travis CI.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
     cd "$TRAVIS_BUILD_DIR"
     git checkout $1
     git reset --hard master
+    git add .
     git -c user.name='travis' -c user.email='travis' commit -m init
     # Make sure to make the output quiet, or else the API token will leak!
     # This works because the API key can replace your password.
