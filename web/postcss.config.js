bec02a1af7402b49ec2c147d12f4c6cf9625ca70
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-12-29 08:16:55 +0000

    :fire: Remove unnecessaary dependencies.

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,36 +1,6 @@
 module.exports = {
   plugins:
-    process.env.NODE_ENV === 'production'
-      ? [
-        'postcss-flexbugs-fixes',
-        [
-          'postcss-preset-env',
-          {
-            autoprefixer: {
-              flexbox: 'no-2009',
-            },
-            stage: 3,
-            features: {
-              'custom-properties': false,
-            },
-          },
-        ],
-      ]
-      : [
-          'precss'
-        // No transformations in development
-      ],
+    [
+        'precss'
+    ],
 }
-// module.exports = ({file, options}) => ({
-//   plugins: {
-//     'postcss-import': {},
-//     cssnano: process.env.NODE_ENV === 'production' ? {} : false,
-//     'postcss-preset-env': {
-//       stage: 0,
-//       features: {
-//         'color-mod-function': {unresolved: 'warn'},
-//         'nesting-rules': true
-//       }
-//     }
-//   }
-// })
